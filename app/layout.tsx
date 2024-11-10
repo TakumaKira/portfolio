bc2b3175bce8b8f663b9b5ccac9d5b2a2b3a5a04
Takuma Kira <takuma@kirakiraworx.com>
2024-11-10 15:30:28 +0900

    [dev]: Add PT serif fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,26 @@ const caveat = localFont({
   variable: "--font-caveat",
   weight: "400 500 600 700",
 });
+const ptSerifRegular = localFont({
+  src: "./fonts/PTSerif-Regular.ttf",
+  variable: "--font-pt-serif",
+  weight: "400",
+});
+const ptSerifBold = localFont({
+  src: "./fonts/PTSerif-Bold.ttf",
+  variable: "--font-pt-serif-bold",
+  weight: "700",
+});
+const ptSerifItalic = localFont({
+  src: "./fonts/PTSerif-Italic.ttf",
+  variable: "--font-pt-serif-italic",
+  weight: "400",
+});
+const ptSerifBoldItalic = localFont({
+  src: "./fonts/PTSerif-BoldItalic.ttf",
+  variable: "--font-pt-serif-bold-italic",
+  weight: "700",
+});
 
 export const metadata: Metadata = {
   title: "Takuma's portfolio",
@@ -24,7 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${permanentMarker.variable} ${caveat.variable}`}>
+      <body className={`${permanentMarker.variable} ${caveat.variable} ${ptSerifRegular.variable} ${ptSerifBold.variable} ${ptSerifItalic.variable} ${ptSerifBoldItalic.variable}`}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
