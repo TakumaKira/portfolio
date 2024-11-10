4946c05815bba71c00e1b01b87d8e2a1070b8016
Takuma Kira <takuma@kirakiraworx.com>
2024-11-10 15:11:57 +0900

    [dev]: Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from "next";
 import StyledComponentsRegistry from './lib/registry'
+import localFont from "next/font/local";
+
+const permanentMarker = localFont({
+  src: "./fonts/PermanentMarker-Regular.ttf",
+  variable: "--font-permanent-marker",
+  weight: "400",
+});
+const caveat = localFont({
+  src: "./fonts/Caveat-VariableFont_wght.ttf",
+  variable: "--font-caveat",
+  weight: "400 500 600 700",
+});
 
 export const metadata: Metadata = {
   title: "Takuma's portfolio",
@@ -12,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${permanentMarker.variable} ${caveat.variable}`}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
