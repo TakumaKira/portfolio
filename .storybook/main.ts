56b261c12460186d40f39293bd6ea0043c11e06c
Takuma Kira <takuma@kirakiraworx.com>
2024-11-16 21:38:12 +0900

    [dev]: Make SVG work on storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,5 +21,38 @@ const config: StorybookConfig = {
       to: 'app/fonts',
     },
   ],
+  webpackFinal(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config?.module?.rules?.find((rule) =>
+      typeof rule === 'object' && typeof rule?.test === 'object' && 'test' in rule.test && typeof rule.test.test === 'function' && rule.test.test('.svg'),
+    )
+
+    if (config?.module?.rules && typeof fileLoaderRule === 'object' && fileLoaderRule !== null) {
+      let not: any[] = []
+      if (typeof fileLoaderRule.resourceQuery === 'object' && 'not' in fileLoaderRule.resourceQuery && Array.isArray(fileLoaderRule.resourceQuery.not)) {
+        not = fileLoaderRule.resourceQuery.not
+      }
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...not, /url/] }, // exclude if *.svg?url
+          use: ['@svgr/webpack'],
+        },
+      )
+      
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i
+    }
+
+    return config
+  },
 };
 export default config;
