1405b4d28773cbe89e37fa7446e4b41bbd176f78
Takuma Kira <takuma@kirakiraworx.com>
2024-11-24 17:40:37 +0900

    [test]: Setup unit testing environment

---
+++ app/hooks/useColorScheme.test.ts
@@ -0,0 +1,5 @@
+describe('useColorScheme', () => {
+  it('should run test', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ app/lib/onChangeCurrentTiming.test.ts
@@ -0,0 +1,5 @@
+describe('onChangeCurrentTiming', () => {
+  it('should run test', () => {
+    expect(true).toBe(true)
+  })
+})
