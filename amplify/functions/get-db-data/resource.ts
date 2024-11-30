01f00166af141c33ede679696b8e086a1d0b1e6a
Takuma Kira <takuma@kirakiraworx.com>
2024-11-30 16:12:58 +0900

    [dev]: Fix type error

--- amplify/functions/get-db-data/resource.ts
+++ amplify/functions/get-db-data/resource.ts
@@ -1,8 +1,13 @@
 import { defineFunction } from '@aws-amplify/backend';
 
+const prismaLambdaLayerArn = process.env.PRISMA_LAMBDA_LAYER_ARN
+if (!prismaLambdaLayerArn) {
+  throw new Error("PRISMA_LAMBDA_LAYER_ARN is not set")
+}
+
 export const getDbData = defineFunction({
   name: 'get-db-data',
   layers: {
-    "portfolio-prisma": process.env.PRISMA_LAMBDA_LAYER_ARN
+    "portfolio-prisma": prismaLambdaLayerArn
   },
 })
