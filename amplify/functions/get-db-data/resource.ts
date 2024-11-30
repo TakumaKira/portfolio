7c2368e08a02cdeb82ddee84cb60d584cb870dc9
Takuma Kira <takuma@kirakiraworx.com>
2024-11-30 16:04:58 +0900

    [dev]: Set actual lambda layer arn

--- amplify/functions/get-db-data/resource.ts
+++ amplify/functions/get-db-data/resource.ts
@@ -3,6 +3,6 @@ import { defineFunction } from '@aws-amplify/backend';
 export const getDbData = defineFunction({
   name: 'get-db-data',
   layers: {
-    "portfolio-prisma": "arn:aws:lambda:us-east-1:123456789012:layer:portfolio-prisma:1"
+    "portfolio-prisma": process.env.PRISMA_LAMBDA_LAYER_ARN
   },
 })
